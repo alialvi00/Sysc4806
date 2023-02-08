befb49ac582721be4f1637be52ddae1a300b40f9
alialvi00 <ali_mashhood98@hotmail.com>
2023-02-08 16:20:22 -0500

    Deleted old unit tests and removed JUnit dependency from maven

--- src/test/java/sysc4806labs/lab3/AddressBookControllerTest.java
+++ src/test/java/sysc4806labs/lab3/AddressBookControllerTest.java
@@ -1,7 +1,6 @@
 package sysc4806labs.lab3;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class AddressBookControllerTest {
